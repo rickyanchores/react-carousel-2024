01f1f88533bde43c60a9713ab6ba505cf317ca91
RickyAnchores <ricky23anchores@gmail.com>
2024-02-25 13:38:27 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Carousel from './components/Carousel/Carousel'
 
 
 const App = () => {
   return (
-    <div className='App min-h-screen flex justify-center items-center'>
-      <div className="container bg-black p-5">
-        <h1>alpha</h1>
-        <p>boiler plate 2024</p>
-        <ul>
-          <li>React</li>
-          <li>Tailwind</li>
-          <li>Scss</li>
-        </ul>
-      </div>
+    <div className='App min-h-screen flex flex-col justify-center items-center'>
+          <Carousel />
     </div>
   )
 }
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Carousel = () => {
+  return (
+    <div className="Carousel bg-gray-800 p-4">
+        <h1 className="text-4xl">React Carousel 2024</h1>
+        <div className="carousel-container">
+            <h1>Card</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Carousel

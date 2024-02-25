e5b10ee77948839aad8b4ba2956225d53035cea8
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2024-02-25 13:30:24 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+const App = () => {
+  return (
+    <div className='App min-h-screen flex justify-center items-center'>
+      <div className="container bg-black p-5">
+        <h1>alpha</h1>
+        <p>boiler plate 2024</p>
+        <ul>
+          <li>React</li>
+          <li>Tailwind</li>
+          <li>Scss</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
